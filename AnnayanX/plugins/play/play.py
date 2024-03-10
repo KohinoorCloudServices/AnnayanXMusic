02753d9b4b40edde87c5afacea078c009e924d06
AnnayanX <148120781+AnnayanX@users.noreply.github.com>
2024-03-11 03:14:31 +0530

    Update play.py

--- AnnayanX/plugins/play/play.py
+++ AnnayanX/plugins/play/play.py
@@ -663,3 +663,25 @@ async def slider_queries(client, CallbackQuery, _):
         return await CallbackQuery.edit_message_media(
             media=med, reply_markup=InlineKeyboardMarkup(buttons)
         )
+@app.on_callback_query(filters.regex("unban_assistant"))
+async def unban_assistant_(_, CallbackQuery):
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    chat_id, user_id = callback_request.split("|")
+    a = await app.get_chat_member(int(chat_id), app.id)
+    if not a.can_restrict_members:
+        return await CallbackQuery.answer(
+            "ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴘᴇʀᴍɪssɪᴏɴs ᴛᴏ ᴜɴʙᴀɴ ᴜsᴇʀs ɪɴ ᴛʜɪs ᴄʜᴀᴛ..",
+            show_alert=True,
+        )
+    else:
+        try:
+            await app.unban_chat_member(int(chat_id), int(user_id))
+        except:
+            return await CallbackQuery.answer(
+                "ғᴀɪʟᴇᴅ ᴛᴏ ᴜɴʙᴀɴ ᴛʜᴇ ᴀssɪsᴛᴀɴᴛ ᴀᴄᴄᴏᴜɴᴛ.",
+                show_alert=True,
+            )
+        return await CallbackQuery.edit_message_text(
+            "ᴀssɪsᴛᴀɴᴛ ᴀᴄᴄᴏᴜɴᴛ ᴜɴʙᴀɴɴᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ.\n\nᴛʀʏ ᴘʟᴀʏɪɴɢ ɴᴏᴡ..."
+        )
